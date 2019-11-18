732393738f4798eb8107b1a362e8c70e5271fb8a
Bino <binonguyen1090@gmail.com>
2019-11-17 22:51:00 -0600

    fix model with image, move to new db, model is working

--- allchefs/models.py
+++ allchefs/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Chef (models.Model):
     name = models.CharField(max_length=200)
-    body = models.TextField
-    image = models.ImageField
+    body = models.TextField(max_length=2000)
+    image = models.ImageField(upload_to='media')
 
     def __str__(self):
         return self.image
--- chef_bucket/settings.py
+++ chef_bucket/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'chef_bucket.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chefbucket',
+        'NAME': 'chefdb',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST':'localhost',
@@ -124,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
