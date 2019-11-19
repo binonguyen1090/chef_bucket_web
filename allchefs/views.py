de7358b3806fe45908bd4df9929cf462165ce313
Bino <binonguyen1090@gmail.com>
2019-11-18 21:41:38 -0600

    Stylist templates

--- allchefs/views.py
+++ allchefs/views.py
@@ -5,6 +5,7 @@ from .models import Chef
 # Create your views here.
 
 def home(request):
+
     allchefs = Chef.objects.all
     return render(request, 'allchefs/home.html', {'allchefs' : allchefs})
 
