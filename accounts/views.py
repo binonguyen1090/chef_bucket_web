45feac956d59db65708027dec4a8a5668ed75334
Bino <binonguyen1090@gmail.com>
2019-11-19 20:44:23 -0600

    Sign Up page is working but Return Home is FAIL

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
-# from django.contrib.auth.models import User
-# from django.contrib import auth
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User already has been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request,user)
+                return render(request, 'allchefs/home.html')
+        else:
+            return render(request, 'accounts/signup.html',{'error': 'Passowrd miss match'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
